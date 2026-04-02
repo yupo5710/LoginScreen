2d683da08972e14100d725cf0866a11b84708e48
ChoiYulDO <yuldo04@gmail.com>
2026-04-02 11:56:49 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace LoginScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblAppName = new Label();
+            txtID = new TextBox();
+            txtPW = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("맑은 고딕", 24F, FontStyle.Bold);
+            lblAppName.Location = new Point(269, 32);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(207, 86);
+            lblAppName.TabIndex = 0;
+            lblAppName.Text = "Login";
+            // 
+            // txtID
+            // 
+            txtID.Font = new Font("맑은 고딕", 13.875F);
+            txtID.ForeColor = Color.FromArgb(224, 224, 224);
+            txtID.Location = new Point(226, 171);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(324, 57);
+            txtID.TabIndex = 1;
+            txtID.Text = "아이디";
+            txtID.Enter += txtID_Enter;
+            txtID.Leave += txtID_Leave;
+            // 
+            // txtPW
+            // 
+            txtPW.Font = new Font("맑은 고딕", 13.875F);
+            txtPW.ForeColor = Color.FromArgb(224, 224, 224);
+            txtPW.Location = new Point(226, 261);
+            txtPW.Name = "txtPW";
+            txtPW.Size = new Size(324, 57);
+            txtPW.TabIndex = 2;
+            txtPW.Text = "패스워드";
+            txtPW.Enter += txtPW_Enter;
+            txtPW.Leave += txtPW_Leave;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(288, 401);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(188, 76);
+            btnLogin.TabIndex = 3;
+            btnLogin.Text = "로그인";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 511);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPW);
+            Controls.Add(txtID);
+            Controls.Add(lblAppName);
+            Name = "Form1";
+            Text = "Login Screen";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblAppName;
+        private TextBox txtID;
+        private TextBox txtPW;
+        private Button btnLogin;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+namespace LoginScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // 정답 정보 (실습 예시: admin & superman)
+            string myID = "admin";
+            string myPW = "superman";
+
+            // 아이디와 패스워드가 모두 맞아야 로그인 성공 (&& 연산자 사용)
+            if (txtID.Text == myID && txtPW.Text == myPW)
+            {
+                // 성공 메시지 박스 출력
+                MessageBox.Show("로그인 성공!");
+            }
+            else
+            {
+                // 실패 시: 메시지 박스 대신 라벨 표시
+                MessageBox.Show("로그인 실패!");
+            }
+        }
+
+        private void txtID_Enter(object sender, EventArgs e)
+        {
+            if (txtID.Text == "아이디") // 플레이스홀더 텍스트가 있을 때만 초기화
+            {
+                txtID.Text = ""; // 글자 사라짐
+                txtID.ForeColor = Color.Black; // 입력 시 검정색
+            }
+        }
+
+        private void txtID_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtID.Text))
+            {
+                txtID.Text = "아이디";
+                txtID.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtPW_Enter(object sender, EventArgs e)
+        {
+            if (txtPW.Text == "패스워드")
+            {
+                txtPW.Text = "";
+                txtPW.ForeColor = Color.Black;
+                txtPW.UseSystemPasswordChar = true; // 입력 시작 시 마스킹 활성화
+
+            }
+        }
+
+        private void txtPW_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPW.Text))
+            {
+                txtPW.UseSystemPasswordChar = false; // 힌트 표시 위해 마스킹 해제
+                txtPW.Text = "패스워드";
+                txtPW.ForeColor = Color.Silver;
+            }
+        }
+    }
+}
